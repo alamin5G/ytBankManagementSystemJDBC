dee7f1eaed48541a71580ae89a3110a963e34022
alamin5g <alaminrakib4864@gmail.com>
2024-02-06 16:13:30 +0600

    BANK MS USING JDBC

--- src/com/goonok/Accounts.java
+++ src/com/goonok/Accounts.java
@@ -1,7 +1,11 @@
 package com.goonok;
 
-import java.sql.Connection;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.Stack;
 
 public class Accounts {
     private Connection connection;
@@ -12,13 +16,83 @@ public class Accounts {
         this.scanner = scanner;
     }
 
-    public boolean accountExist(String email) {
-
-        return false;
-    }
 
     public long openAccount(String email) {
+        if (!accountExist(email)){
+            String openAccountQuery = "INSERT INTO accounts(account_number, full_name, email,balance, security_pin) VALUES (?, ?, ?, ?, ?)";
+            scanner.nextLine();
+            System.out.print("Enter full name: ");
+            String fullName = scanner.nextLine();
+            System.out.print("Enter Initial Amount: ");
+            double balance = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("Enter 4 digit PIN: ");
+            String securityPin = scanner.nextLine();
 
+            try {
+                long accountNumber = generateAccountNumber();
+                PreparedStatement preparedStatement = connection.prepareStatement(openAccountQuery);
+                preparedStatement.setLong(1, accountNumber);
+                preparedStatement.setString(2, fullName);
+                preparedStatement.setString(3, email);
+                preparedStatement.setDouble(4, balance);
+                preparedStatement.setString(5, securityPin);
+                int affectedRows = preparedStatement.executeUpdate();
+                if (affectedRows> 0){
+                    return accountNumber;
+                }else {
+                    System.out.println("Account creation failed!");
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
         return 0L;
+    }
+
+    public long generateAccountNumber() {
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT account_number FROM accounts ORDER BY account_number DESC LIMIT 1");
+            if (resultSet.next()){
+                long lastAccountNumber = resultSet.getLong("account_number");
+                return lastAccountNumber+1;
+            }else {
+                return 10000100;
+            }
+        }catch (SQLException s){
+            System.err.println("Connection failed at generateAccountNumber() -" + s.getMessage());
+        }
+        return 10000100;
+    }
+
+    public long getAccountNumber(String email){
+        String query = "SELECT account_number FROM accounts WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                return resultSet.getLong("account_number");
+            }
+        }catch (SQLException s){
+            System.err.println("Connection failed at getAccountNumber() - " + s.getMessage());;
+        }
+        return 0;
+    }
+
+    public boolean accountExist(String email) {
+        String checkQuery = "SELECT account_number FROM accounts WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(checkQuery);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        }catch (SQLException s){
+            System.err.println("Connection failed at accountExist() - " + s.getMessage());
+        }
+        return false;
     }
 }
--- src/com/goonok/User.java
+++ src/com/goonok/User.java
@@ -3,6 +3,7 @@ package com.goonok;
 import javax.security.auth.login.CredentialNotFoundException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -26,22 +27,22 @@ public class User {
         if (userExist(email)){
             System.out.println("User already exists for this account!");
             return;
-        }
-
-        String registerQuery = "INSERT INTO User(full_name, email, password) VALUES (?, ?, ?)";
-        try{
-            PreparedStatement preparedStatement = connection.prepareStatement(registerQuery);
-            preparedStatement.setString(1, fullName);
-            preparedStatement.setString(2, email);
-            preparedStatement.setString(3, password);
-            int affectedRows = preparedStatement.executeUpdate();
-            if (affectedRows>0){
-                System.out.println("Registration Successful!");
-            }else {
-                System.out.println("Registration Failed!");
+        }else {
+            String registerQuery = "INSERT INTO User(full_name, email, password) VALUES (?, ?, ?)";
+            try{
+                PreparedStatement preparedStatement = connection.prepareStatement(registerQuery);
+                preparedStatement.setString(1, fullName);
+                preparedStatement.setString(2, email);
+                preparedStatement.setString(3, password);
+                int affectedRows = preparedStatement.executeUpdate();
+                if (affectedRows>0){
+                    System.out.println("Registration Successful!");
+                }else {
+                    System.out.println("Registration Failed!");
+                }
+            }catch (SQLException s){
+                System.out.println("Connection failed at - User.register() method + " + s.getMessage());
             }
-        }catch (SQLException s){
-            System.out.println("Connection failed at - User.register() method + " + s.getMessage());
         }
 
     }
@@ -51,11 +52,40 @@ public class User {
 
     public String login() {
 
+        scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        String loginQuery = "SELECT * FROM user WHERE email = ? AND password = ?";
+
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(loginQuery);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                return  email;
+            }else {
+                return null;
+            }
+        }catch (SQLException s){
+            System.out.println("Connection failed at login() - " + s.getMessage());
+        }
+
         return "";
     }
 
     private boolean userExist(String email) {
-
+        String query = "SELECT * FROM user WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        }catch (SQLException s){
+            System.err.println("Connection failed at userExist() method - " + s.getMessage());
+        }
         return false;
     }
 }
